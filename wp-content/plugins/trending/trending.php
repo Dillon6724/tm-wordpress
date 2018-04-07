036f12bf0e61e05de811ff2454305fce9caa8ac1
Dillon <dmcguire@emmisdigital.com>
2018-04-07 15:22:34 -0500

    trending function added

--- wp-content/plugins/trending/trending.php
+++ wp-content/plugins/trending/trending.php
@@ -3,4 +3,12 @@
 /*
 Plugin Name: Trending Articles
 */
+
+add_action( 'init', 'add_trending' );
+
+
+function add_trending()
+{
+  echo "<p style='color: black;'>This is my trending articles widget</p>";
+}
 ?>
