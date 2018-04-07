406d9b6cdd9a5c278fd326e381d500ab2155533e
Dillon <dmcguire@emmisdigital.com>
2018-04-07 16:01:17 -0500

    added action to plugin

--- wp-content/plugins/trending.php
+++ wp-content/plugins/trending.php
@@ -11,6 +11,9 @@ class trending extends WP_Widget {
     $control_ops = array( 'id_base' => 'trending' ); // Widget Control Settings
     $this->WP_Widget( 'trending', 'Trending Articles', $widget_ops, $control_ops ); // Create the widget
   }
-  
+
 }
+
+add_action('widgets_init', create_function('', 'return register_widget("trending");'));
+
 ?>
