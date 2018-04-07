c7dd6c6440c631f70198d95d9fd5f859c53fcf7a
Dillon <dmcguire@emmisdigital.com>
2018-04-07 16:43:42 -0500

    api call example

--- wp-content/plugins/trending.php
+++ wp-content/plugins/trending.php
@@ -13,8 +13,16 @@ class trending extends WP_Widget {
   }
 
   function widget($args, $instance) {
-    echo "<p style='color: pink;'>Here are te trending articles</p>";
-  }
+    // create curl resource
+    $ch = curl_init();
+    // set url
+    curl_setopt($ch, CURLOPT_URL, "https://api.genderize.io/?name=Dillon");
+    // $output contains the output json
+    $output = curl_exec($ch);
+    // close curl resource to free up system resources
+    curl_close($ch);
+    // {"name":"Baron","gender":"male","probability":0.88,"count":26}
+    echo var_dump(json_decode($output, true));  }
 }
 
 add_action('widgets_init', create_function('', 'return register_widget("trending");'));
