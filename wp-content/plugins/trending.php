5a843da99e25941320a2952c3910210e62fb9998
Dillon <dmcguire@emmisdigital.com>
2018-04-07 15:54:14 -0500

    trending function actaually added

--- wp-content/plugins/trending.php
+++ wp-content/plugins/trending.php
@@ -5,8 +5,12 @@ Plugin Name: Trending Articles
 */
 
 class trending extends WP_Widget {
-  $widget_ops = array( 'classname' => 'trending', 'description' => 'Displays your upcoming posts to tease your readers' ); // Widget Settings
-  $control_ops = array( 'id_base' => 'trending' ); // Widget Control Settings
-  $this->WP_Widget( 'trending', 'Trending Articles', $widget_ops, $control_ops ); // Create the widget
+
+  function trending() {
+    $widget_ops = array( 'classname' => 'trending', 'description' => 'Displays your upcoming posts to tease your readers' ); // Widget Settings
+    $control_ops = array( 'id_base' => 'trending' ); // Widget Control Settings
+    $this->WP_Widget( 'trending', 'Trending Articles', $widget_ops, $control_ops ); // Create the widget
+  }
+  
 }
 ?>
