50779bb2c597def59d09c911577b933738d0ec30
Dillon <dmcguire@emmisdigital.com>
2018-04-07 22:17:50 -0500

    removed api call to test return

--- wp-content/plugins/trending.php
+++ wp-content/plugins/trending.php
@@ -15,7 +15,7 @@ class trending extends WP_Widget {
   function widget($args, $instance) {
     $response = $this->get_parsley_data();
     $titles = 'my titles: ';
-    foreach ($response->data as $article) {
+    foreach ($response->$data as $article) {
       $titles.$article['title'];
     };
     echo $titles->data;
