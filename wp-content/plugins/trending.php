50c91eba0fde1ebe3d16a0f10ca73f9e1ad62358
Dillon <dmcguire@emmisdigital.com>
2018-04-07 23:16:36 -0500

    updated concat

--- wp-content/plugins/trending.php
+++ wp-content/plugins/trending.php
@@ -27,7 +27,7 @@ class trending extends WP_Widget {
     foreach ($response->data as $article) {
       $titles.$article->title;
     }
-    echo gettype($response);
+    echo $response;
   }
 
   function get_parsley_data() {
