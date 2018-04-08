31ef8cb2687b221d80d5bef7bc0c91235cddd921
Dillon <dmcguire@emmisdigital.com>
2018-04-07 23:28:27 -0500

    changed json decode to obj

--- wp-content/plugins/trending.php
+++ wp-content/plugins/trending.php
@@ -25,7 +25,7 @@ class trending extends WP_Widget {
     $titles = "Titles: ";
 
     foreach ($response->data as $article) {
-      $titles.$article->title;
+      echo $article->title;
     }
 
     echo count($response->data);
