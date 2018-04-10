afef64fe8e55f5ef4c6e32d5c9f1250d038c964f
Dillon <dmcguire@emmisdigital.com>
2018-04-10 15:01:51 -0500

    admin template

--- wp-content/plugins/trending/trending.php
+++ wp-content/plugins/trending/trending.php
@@ -41,18 +41,6 @@ class trending extends WP_Widget {
     return json_decode($output);
   }
 
-
-  // function update($new_instance, $old_instance) {
-  //   $instance['title'] = strip_tags($new_instance['title']);
-  //   $instance['soup_number'] = strip_tags($new_instance['soup_number']);
-  //   $instance['post_type'] = $new_instance['post_type'];
-  //   $instance['show_newsletter'] = $new_instance['show_newsletter'];
-  //   $instance['newsletter_url'] = strip_tags($new_instance['newsletter_url'],'<a>');
-  //   $instance['author_credit'] = $new_instance['author_credit'];
-  //   return $instance;
-  // }
-
-  // Widget Control Panel //
   function form($instance) {
     echo "<h1>this is in my control panel</h1>";
   }
