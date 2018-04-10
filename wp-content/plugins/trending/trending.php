21ff1282e524ef3d31ec5e5f5a87eaa28632755b
Dillon <dmcguire@emmisdigital.com>
2018-04-10 16:01:28 -0500

    new filter on widget article max variable

--- wp-content/plugins/trending/trending.php
+++ wp-content/plugins/trending/trending.php
@@ -16,7 +16,7 @@ class trending extends WP_Widget {
 
   function widget($args, $instance) {
     extract( $args );
-    $article_max = $instance['article-max'] || 10;
+    $article_max = apply_filters( 'article-max', $instance[ 'article-max' ] );
     $response = $this->get_parsley_data($article_max);
     $ranking = 1;
     echo "MAXXX: ".$article_max;
