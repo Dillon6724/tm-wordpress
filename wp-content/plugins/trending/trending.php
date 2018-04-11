4e88e31db44675d5338c3b7c7909eb55a367c984
Dillon <dmcguire@emmisdigital.com>
2018-04-10 20:51:23 -0500

    got rid of settings page

--- wp-content/plugins/trending/trending.php
+++ wp-content/plugins/trending/trending.php
@@ -46,7 +46,8 @@ class trending extends WP_Widget {
     return json_decode($output);
   }
 
-  function update($instance, $old_instance) {
+  function update($new_instance, $old_instance) {
+    $instance = $old_instance;
     $instance['title'] = strip_tags($new_instance['title']);
     return $instance;
   }
