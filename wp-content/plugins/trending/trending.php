5f3b305382d6a141efa3b998b1a772745e8c5439
Dillon <dmcguire@emmisdigital.com>
2018-04-10 21:02:32 -0500

    added extra php tag

--- wp-content/plugins/trending/trending.php
+++ wp-content/plugins/trending/trending.php
@@ -56,6 +56,7 @@ class trending extends WP_Widget {
     $title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';?>
     <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+  <?php
   }
 }
 
